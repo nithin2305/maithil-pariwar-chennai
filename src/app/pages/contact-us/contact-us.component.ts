cea38979c1a257fabf14855c67803c6050099e62
nithin2305 <nithinmishra99@gmail.com>
2025-04-19 22:43:05 +0530

    form updation

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent {
+name: string = '';
+email: string = '';
+message: string = '';
 
-}
+  onSubmit() {
+    if (!this.name || !this.email || !this.message) {
+      alert('Please fill in all fields.');
+      return;
+      }
+    if (!this.validateEmail(this.email)) {
+      alert('Please enter a valid email address.');
+      return;
+    }
+    alert ('Thanks for your submission! We will get back to you soon.');
+    this.name = '';
+    this.email = '';
+    this.message = '';
+  }
+    // Reset the form or perform any other actions as needed
+    validateEmail(email: string): boolean {
+      const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return re.test(String(email).toLowerCase());
+    }
+  }
+
+  
+
